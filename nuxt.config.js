df3a21baf20c8b958c768ebfab3801b3472ce646
Tran Duc <duccck92@gmail.com>
2021-01-12 16:29:21 +0700

    remove purge css

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'nuxt-news',
+    title: 'nuxt-news-no-purgecss',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -20,7 +20,9 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: ['nuxt-purgecss'],
+  buildModules: [
+    // 'nuxt-purgecss'
+  ],
 
   purgeCSS: {
     paths: [
